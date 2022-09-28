ace67704206ae787ee8f3892d0d77ee48157e7ea
sergiohidalgo818 <78908100+sergiohidalgo818@users.noreply.github.com>
2022-09-28 10:23:03 +0200

    Add files via upload
    OLA

--- p1/practica1.py
+++ p1/practica1.py
@@ -16,7 +16,6 @@ import argparse
 from argparse import RawTextHelpFormatter
 import time
 import logging
-from datetime import datetime
 
 ETH_FRAME_MAX = 1514
 PROMISC = 1
@@ -29,19 +28,10 @@ def signal_handler(nsignal,frame):
 	logging.info('Control C pulsado')
 	if handle:
 		pcap_breakloop(handle)
-		
-def analizar_traza(us,header,data):
-	global num_paquete, pdumper
-	logging.info('Nuevo paquete de {} bytes capturado en el timestamp UNIX {}.{}'.format(header.len,header.ts.tv_sec,header.ts.tv_sec))
-	num_paquete += 1
-	
-	#imprimir los N primeros bytes
-	print (binascii.hexlify(data[:args.nbytes]))
-	
 
 
 def procesa_paquete(us,header,data):
-	global num_paquete, pdumper
+	global num_paquete, pdumper, descr
 	logging.info('Nuevo paquete de {} bytes capturado en el timestamp UNIX {}.{}'.format(header.len,header.ts.tv_sec,header.ts.tv_sec))
 	num_paquete += 1
 	
@@ -49,19 +39,15 @@ def procesa_paquete(us,header,data):
 	print (binascii.hexlify(data[:args.nbytes]))
 	
 	#Escribir el tráfico al fichero de captura con el offset temporal
-	descr = pcap_open_dead(DLT_EN10MB, ETH_FRAME_MAX)
 	
 	#la traza debe ser solo cndo empieza o cada vez q pasa un segundo?
-	#pdumper = pcap_dump_open(descr, 'captura.'+ str(args.interface) + '.' + str(header.ts.tv_sec) +'.pcap')
-	pdumper = pcap_dump_open(descr, 'captura.'+ str(args.interface) + '.' + str(TIME_OFFSET) +'.pcap')
-	
-	pcap_dump(pdumper, header, data)
+	if args.interface != False:
+		pcap_dump(pdumper, header, data)
 
-	pcap_close(descr)
 
 	
 if __name__ == "__main__":
-	global pdumper,args,handle
+	global pdumper,args,handle,descr
 	parser = argparse.ArgumentParser(description='Captura tráfico de una interfaz ( o lee de fichero) y muestra la longitud y timestamp de los 50 primeros paquetes',
 	formatter_class=RawTextHelpFormatter)
 	parser.add_argument('--file', dest='tracefile', default=False,help='Fichero pcap a abrir')
@@ -92,13 +78,14 @@ if __name__ == "__main__":
 	if args.interface != False:
 		handle = pcap_open_live(args.interface, ETH_FRAME_MAX, NO_PROMISC, TO_MS, errbuf)
 
+	descr = pcap_open_dead(DLT_EN10MB, ETH_FRAME_MAX)
+	pdumper = pcap_dump_open(descr, 'captura.' + str(args.interface) + '.' + str(int(time.time())) +'.pcap')
 
 	#abrir un dumper para volcar el tráfico (si se ha especificado interfaz) 
 	if args.tracefile != False:
 		handle = pcap_open_offline(args.tracefile, errbuf)
-		ret = pcap_loop(handle,50,analizar_traza,None)
-	else:
-		ret = pcap_loop(handle,50,procesa_paquete,None)
+
+	ret = pcap_loop(handle,50,procesa_paquete,None)
 	
 	if ret == -1:
 		logging.error('Error al capturar un paquete')
@@ -108,6 +95,9 @@ if __name__ == "__main__":
 		logging.debug('No mas paquetes o limite superado')
 	logging.info('{} paquetes procesados'.format(num_paquete))
 	
+	if descr !=None:
+		pcap_close(descr)
+
 	#si se ha creado un dumper cerrarlo
 	if pdumper != None:
 		pcap_dump_close(pdumper)
