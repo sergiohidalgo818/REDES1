a72e741735043cae18fdc2ab344b66a810cacbdf
sergiohidalgo818 <sergiohg818@gmail.com>
2022-10-04 22:53:53 +0200

    bucle

--- p1/practica1.py
+++ p1/practica1.py
@@ -42,7 +42,7 @@ def procesa_paquete(us,header,data):
 
 	i = 0
 
-	while i <= int(args.nbytes)+1:
+	while i < int(args.nbytes):
 		print(my_data_str[i], end = '')
 		if i %2 != 0:
 			print(' ', end = '')
